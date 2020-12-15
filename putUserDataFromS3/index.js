ac8a52e354a50a2aaaced70c646cea8829ac8764
karemjouini <karemjouini97@gmail.com>
2020-12-15 14:45:01 +0100

    completed lambda transfer from s3 to dynamodb

--- putUserDataFromS3/index.js
+++ putUserDataFromS3/index.js
@@ -13,31 +13,45 @@ exports.handler = async (event) => {
     Key: key,
   };
 
+  let statusCode = 0;
+  let responseBody = "";
+
   try {
     const s3Data = await s3.getObject(getObjectparams).promise();
     const usersStr = s3Data.Body.toString();
     const usersJSON = JSON.parse(usersStr);
-    console.log("userStr", usersStr);
+    await Promise.all(
+      usersJSON.map(async (user) => {
+        const { id, firstname, lastname } = user;
 
-    const { id, firstname, lastname } = usersJSON[0];
+        const putParams = {
+          TableName: "Users",
+          Item: {
+            id: id,
+            firstname: firstname,
+            lastname: lastname,
+          },
+        };
+        try {
+          const putItemData = await documentClient.put(putParams).promise();
+        } catch (error) {
+          responseBody = "Can't put users in DynamoDB";
+          statusCode = 403;
+          console.log(error);
+        }
+      })
+    );
 
-    const putParams = {
-      TableName: "Users",
-      Item: {
-        id: id,
-        firstname: firstname,
-        lastname: lastname,
-      },
-    };
-    try {
-      const putItemData = await documentClient.put(putParams).promise();
-      console.log(putItemData);
-    } catch (error) {
-      console.log("Can't put users in DynamoDB");
-      console.log(error);
-    }
+    responseBody = "Succeeded putting users in DynamoDB";
+    statusCode = 201;
   } catch (error) {
-    console.log("Can't get users from s3");
+    responseBody = "Can't get users from s3";
+    statusCode = 403;
     console.log(error);
   }
+  const response = {
+    statusCode: statusCode,
+    body: responseBody,
+  };
+  return response;
 };
